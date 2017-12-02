06c981edb92960033d5d1c83046726474848adea
jlab82 <joseluis@identifica-t.com>
2017-12-02 10:27:09 +0100

    Conversor de unidades de kilómetros a millas

--- Unit_Converter.py
+++ Unit_Converter.py
@@ -5,7 +5,7 @@ yes = True
 while yes:
     km = float(raw_input("Por favor, introduzca número de kilómetros: "))
     milla = km * 0.6214
-    print str(km) + " kilómetros corresponden a " + str(milla) + " millas"
+    print str(km) + " kilómetros corresponden a " + str(milla) + " millas."
     respuesta = str.lower(raw_input("¿Quiere realizar otra coversión? si/no: "))
     if respuesta == "no":
         yes = False
