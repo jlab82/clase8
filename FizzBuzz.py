f1ab7a8bbc10fe66030c7ebb65cda1ccf63f8f86
jlab82 <joseluis@identifica-t.com>
2017-12-02 10:21:57 +0100

    Juego FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-num = int(raw_input("Bienvenidos a FizzBuzz. Por favor, selecciona un número del 1 al 100: "))
+num = int(raw_input("Bienvenidos a FizzBuzz. Por favor, seleccione un número del 1 al 100: "))
 for i in range(num):
     if (i+1) % 3 == 0 and (i+1) % 5 == 0:
         print "fizzbuzz"
