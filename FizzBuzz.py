3672ec24d719737e49f6bb19846b5347784ca55b
jlab82 <joseluis@identifica-t.com>
2017-12-02 10:19:50 +0100

    Conversor de KM a Millas

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+num = int(raw_input("Bienvenidos a FizzBuzz. Por favor, selecciona un número del 1 al 100: "))
+for i in range(num):
+    if (i+1) % 3 == 0 and (i+1) % 5 == 0:
+        print "fizzbuzz"
+    elif (i+1) % 3 == 0:
+        print "fizz"
+    elif (i+1) % 5 == 0:
+        print "buzz"
+    else:
+        print i+1
