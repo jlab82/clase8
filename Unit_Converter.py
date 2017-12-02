ba1c88d4afac04f690b09832d170f0466ab42889
jlab82 <joseluis@identifica-t.com>
2017-12-02 09:55:29 +0100

    Conversor de KM a Millas

---
+++ Unit_Converter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+print "Bienvenido/a. Esta aplicación es una calculadora conversora de kilómetros a millas"
+yes = True
+while yes:
+    km = float(raw_input("Por favor, introduzca número de kilómetros: "))
+    milla = km * 0.6214
+    print str(km) + " kilómetros corresponden a " + str(milla) + " millas"
+    respuesta = raw_input("¿Quiere realizar otra coversión? si/no: ")
+    if respuesta == "no":
+        yes = False
+    elif respuesta =="si":
+        yes = True
+    else:
+        print "Respuesta no válida"
+        break
+print "Muchas gracias por utilizar esta aplicación. FIN"
+
